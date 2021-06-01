316c73c89b554993ed2dcea04d6fb6a3fd1bb9f3
Vit Tomica <vit.tomica@broadcom.com>
2021-06-01 06:12:26 -0400

    Add tests
    Signed-off-by: Vit Tomica <vit.tomica@broadcom.com>

---
+++ test/test.js
@@ -0,0 +1,188 @@
+const spawnSync = require('child_process').spawnSync;
+const expect = require('chai').expect;
+const keyring = require('../');
+
+const username = require('os').userInfo().username;
+const keyringName = 'TESTRING'
+const certAlias = 'testalias'
+const usage = 'PERSONAL'
+const status = 'TRUST'
+
+describe('Test keyring_js', function () {
+  this.timeout(0);
+
+  before("set up", function() {
+    buildKeyringUtil();
+    createAndSetKeyring();
+  });
+
+  it('listKeyring(): verify keyring content', function () {
+    result = keyring.listKeyring(username, keyringName);
+    expect(result).to.have.lengthOf(1);
+    expect(result[0].label).to.equal(certAlias);
+    expect(result[0].owner).to.equal(username);
+    expect(result[0].usage).to.equal(usage);
+    expect(result[0].status).to.equal(status);
+    expect(result[0].default).to.equal(false);
+  });
+
+  it('listKeyring(): verify not enough parameters', function () {
+    try {
+      keyring.listKeyring(username);
+    } catch(e) {
+      expect(e.message).to.equal('Second parameter has wrong argument type');
+    }
+  });
+
+  it('listKeyring(): verify keyring does not exist', function () {
+    try {
+      keyring.listKeyring(username, 'NOTEXIST');
+    } catch(e) {
+      expect(e.message).to.equal('R_datalib call failed: function code: 01, SAF rc: 8, RACF rc: 8, RACF rsn: 84\n');
+    }
+  });
+
+  it('listKeyring(): verify userid does not exist', function () {
+    try {
+      keyring.listKeyring('UNKNWN99', keyringName);
+    } catch(e) {
+      expect(e.message).to.equal('R_datalib call failed: function code: 01, SAF rc: 8, RACF rc: 8, RACF rsn: 84\n');
+    }
+  });
+
+  it('listKeyring(): verify userid name too long', function () {
+    try {
+      keyring.listKeyring('LOOOOONGNAME', keyringName);
+    } catch(e) {
+      expect(e.message).to.equal('First parameter is too long. Parameter can be up to 8 bytes long');
+    }
+  });
+
+  it('listKeyring(): verify keyring name too long', function () {
+    try {
+      keyring.listKeyring(username, 'looooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'oooooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'oooooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'oooooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'ooooooooooooooooooooooongKeyringLabel');
+    } catch(e) {
+      expect(e.message).to.equal('Second parameter is too long. Parameter can be up to 236 bytes long');
+    }
+  });
+
+  it('getPemEncodedData(): verify returned data', function () {
+    result = keyring.getPemEncodedData(username, keyringName, certAlias);
+    expect(result.key).to.contain('-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdw');
+    expect(result.key).to.contain('-----END PRIVATE KEY-----');
+    expect(result.certificate).to.contain('-----BEGIN CERTIFICATE-----\nMIICFTCCAbmgAwIBAgIEFajL/jAMBggqhkjOPQQDAgUAMH8xCzAJBgNVBAYTAkNa\nMQ8wDQY');
+    expect(result.certificate).to.contain('-----END CERTIFICATE-----');
+  });
+
+  it('getPemEncodedData(): verify not enough parameters', function () {
+    try {
+      keyring.getPemEncodedData();
+    } catch(e) {
+      expect(e.message).to.equal('First parameter has wrong argument type');
+    }
+  });
+
+  it('getPemEncodedData(): verify not enough parameters', function () {
+    try {
+      keyring.getPemEncodedData(username);
+    } catch(e) {
+      expect(e.message).to.equal('Second parameter has wrong argument type');
+    }
+  });
+
+  it('getPemEncodedData(): verify not enough parameters', function () {
+    try {
+      keyring.getPemEncodedData(username, keyringName);
+    } catch(e) {
+      expect(e.message).to.equal('Third parameter has wrong argument type');
+    }
+  });
+
+  it('getPemEncodedData(): verify keyring does not exist', function () {
+    try {
+      keyring.getPemEncodedData(username, 'NOTEXIST', certAlias);
+    } catch(e) {
+      expect(e.message).to.equal('R_datalib call failed: function code: 01, SAF rc: 8, RACF rc: 8, RACF rsn: 84\n');
+    }
+  });
+
+  it('getPemEncodedData(): verify userid does not exist', function () {
+    try {
+      keyring.getPemEncodedData('UNKNWN99', keyringName, certAlias);
+    } catch(e) {
+      expect(e.message).to.equal('R_datalib call failed: function code: 01, SAF rc: 8, RACF rc: 8, RACF rsn: 84\n');
+    }
+  });
+
+  it('getPemEncodedData(): verify certificate label does not exist', function () {
+    try {
+      keyring.getPemEncodedData(username, keyringName, 'certdoesnotexist');
+    } catch(e) {
+      expect(e.message).to.equal('R_datalib call failed: function code: 01, SAF rc: 8, RACF rc: 8, RACF rsn: 44\n');
+    }
+  });
+
+  it('getPemEncodedData(): verify userid name too long', function () {
+    try {
+      keyring.getPemEncodedData('LOOOOONGNAME', keyringName, certAlias);
+    } catch(e) {
+      expect(e.message).to.equal('First parameter is too long. Parameter can be up to 8 bytes long');
+    }
+  });
+
+  it('getPemEncodedData(): verify keyring name too long', function () {
+    try {
+      keyring.getPemEncodedData(username, 'looooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'oooooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'oooooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'oooooooooooooooooooooooooooooooooooooooooooooooooo'
+                                  + 'ooooooooooooooooooooooongKeyringLabel', certAlias);
+    } catch(e) {
+      expect(e.message).to.equal('Second parameter is too long. Parameter can be up to 236 bytes long');
+    }
+  });
+
+  it('getPemEncodedData(): verify certificate label too long', function () {
+    try {
+      keyring.getPemEncodedData(username, keyringName, 'looooooooooooooooooooooooongLabel');
+    } catch(e) {
+      expect(e.message).to.equal('Third parameter is too long. Parameter can be up to 32 bytes long');
+    }
+  });
+
+  after("tear down",function() {
+    cleanUpKeyringAndCert();
+  });
+});
+
+function createAndSetKeyring() {
+  executeKeyringUtil(['NEWRING', username, keyringName]);
+  executeKeyringUtil(['IMPORT', username, keyringName, certAlias, usage, 'testcert.p12', 'password']);
+}
+
+function cleanUpKeyringAndCert() {
+  executeKeyringUtil(['DELRING', username, keyringName]);
+  executeKeyringUtil(['DELCERT', username, '*', certAlias]);
+}
+
+function buildKeyringUtil() {
+  res = spawnSync('build.sh',[""],{
+    cwd: './keyring-util'
+  });
+  console.log(res.stdout.toString());
+  console.log(res.stderr.toString());
+}
+
+function executeKeyringUtil(param) {
+  console.log('keyring-util ' + param.toString().replace(/,/g,' '));
+  res = spawnSync('keyring-util', param, {
+    cwd: './keyring-util'
+  });
+  console.log(res.stdout.toString());
+  console.log(res.stderr.toString());
+}
+
